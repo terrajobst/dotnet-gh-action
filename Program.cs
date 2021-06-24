6a4f21b1f1180431bfcc26eca34b959426a6fc90
Immo Landwerth <immo@landwerth.net>
2021-06-24 10:45:49 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_gh_action
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
